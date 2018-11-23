fca6dfcbedd002a0e0781008d87530660827127e
Kyle <kylemanv@aol.com>
2018-11-23 12:20:04 -0700

    new repo and project
    Created new school repository and started a new school project

---
+++ Graphs/Graphs.java
@@ -0,0 +1,16 @@
+/*
+Project:
+
+compilation:
+	graphspackage imports: javac ./graphspackage/*.java
+	main: javac graphs.java
+execution:
+	java Graphs.java
+*/
+import graphspackage.AdjacencyMatrix;
+import graphspackage.AdjacencyList;
+public class Graphs{
+	public static void main(String[] args){
+
+	}
+}
---
+++ Graphs/graphspackage/AdjacencyList.java
@@ -0,0 +1,42 @@
+package graphspackage;
+public class AdjacencyList{
+	public static void hasEdge(){
+
+	}
+	public static void addEdge(){
+
+	}
+	public static void deleteEdge(){
+
+	}
+	public static void addVertex(){
+
+	}
+	public static void deleteVertex(){
+		
+	}
+	public static void isSparse(){
+		
+	}
+	public static void isDense(){
+		
+	}
+	public static void countVertices(){
+		
+	}
+	public static void countEdges(){
+		
+	}
+	public static void isConnected(){
+		
+	}
+	public static void isFullyConnected(){
+		
+	}
+	public static void readGraph(){
+		
+	}
+	public static void printGraph(){
+		
+	}
+}
